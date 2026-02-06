e3c8183e30482f7383bb83cf99df4f444da6bc2b
Assane Sarr <assanesarr030@gmail.com>
2026-02-06 16:55:32 +0000

    add versement

--- app/api/dossiers/route.ts
+++ app/api/dossiers/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
 
     // console.log('Received data:', snapshot.docs.map(doc => doc.data()));
     return NextResponse.json(
-        snapshot.docs.map(doc => ({id: doc.id, ...doc.data()})),
+        // snapshot.docs.map(doc => ({id: doc.id, ...doc.data()})),
         { status: 200 }
     )
 }
