e5389590b216aa308fb023f51f639d5bc79871cb
Assane Sarr <assanesarr030@gmail.com>
2026-02-06 16:36:32 +0000

    add versement

--- app/api/dossiers/delete-dossier/route.ts
+++ app/api/dossiers/delete-dossier/route.ts
@@ -4,24 +4,24 @@ import { NextResponse } from "next/server";
 
 
 export async function DELETE(req: Request) {
-//   try {
-     const { dossierId } = await req.json();
+    //   try {
+    const { dossierId } = await req.json();
 
     if (!dossierId) {
-      return NextResponse.json(
-        { message: "Missing params" },
-        { status: 400 }
-      );
+        return NextResponse.json(
+            { message: "Missing params" },
+            { status: 400 }
+        );
     }
 
-//    const res = await adminDb.collection("dossiers")
-//    .doc(dossierId)
-//    .delete()
+    const res = await adminDb.collection("dossiers")
+        .doc(dossierId)
+        .delete()
 
     // console.log('Received data:', res.docs.map(doc => ({ id: doc.id, ...doc.data() })));
 
     // await admin
-   
+
     //   .collection("dossiers")
     //   .doc(dossierId)
     //   .collection("versements")
@@ -30,13 +30,13 @@ export async function DELETE(req: Request) {
 
     // revalidatePath("/dashboard/clients")
 
-//     return NextResponse.json({ success: true });
-//   } catch (error) {
-//     console.error(error);
-    
-//     return NextResponse.json(
-//       { message: "Server error" },
-//       { status: 500 }
-//     );
-//   }
+    //     return NextResponse.json({ success: true });
+    //   } catch (error) {
+    //     console.error(error);
+
+    //     return NextResponse.json(
+    //       { message: "Server error" },
+    //       { status: 500 }
+    //     );
+    //   }
 }
